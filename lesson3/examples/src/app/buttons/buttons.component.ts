b3d26bc2c4548c061cc988739dd2e69f1b15bee4
Caleb Roman <caleb.l.roman@gmail.com>
2021-08-18 16:03:24 -0500

    angular pt 3 exercises

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
    inactive: boolean = false;
+   buttonOnOff: boolean[] = [true, true, true]
 
    constructor() { }
 
    ngOnInit() { }
 
+   activateButton() {
+      for (let i=0;i < this.buttonOnOff.length; i++) {
+         this.buttonOnOff[i] = true;
+      }
+   }
 }
